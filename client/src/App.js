15db581fe83ec32fb3b2856a12440279653fd740
Paul LaBounty <labountypaul@icloud.com>
2021-03-11 17:06:16 -0600

    create proxy to run both front end and back end servers

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Collaborators from './components/collaborators/collaborators';
+
+function App() {
+  return (
+    <div>
+      <Collaborators />
+    </div>
+  );
+}
+
+export default App;
